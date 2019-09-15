bddd78846a5c9f7125109247bbe2e3b5c50b0ee3
ivanhercaz <ivan@ivanhercaz.com>
2019-09-15 22:47:03 +0100

    Reorganize imports (#8)
    Reorganization of the imports: some of them has been removed because now
    they are unused and the rest has been reorganized.

--- app.py
+++ app.py
@@ -10,16 +10,10 @@
 """
 
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.layout import Layout
-from kivy.uix.widget import Widget
-from kivy.core.window import Window
 from kivy.config import Config
-
-from kivy.properties import ListProperty
+from kivy.core.window import Window
 from kivy.properties import NumericProperty, ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
 
 import yaml
 
